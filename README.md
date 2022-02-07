6c651a228d88758dad9472d2ce593e311c420034
wangjs-jacky <2409277719@qq.com>
2022-02-08 07:33:14 +0800

    使用node搭建静态服务器(如果才能在服务器端获取curl -v中的内容)

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # node-server
 使用node搭建静态服务器
+
+## 使用方式
+### 1. 终端输入
+```shell
+npm run dev
+```
+
+### 2. 客户端
+
+2.1 终端输入
+```shell
+# -v 客户端打印出 请求头(终端>)+响应体(终端<)
+curl -v http://localhost:8888
+
+# -d 可以将 GET 请求转变为 POST请求
+curl -v -d "name=wangjiasheng" http://localhost:8888
+```
+
+2.2 浏览器发出GET请求
+在chrome 直接输入 http://localhost:8888
---
+++ index.ts
@@ -0,0 +1,41 @@
+import * as http from "http";
+const server = http.createServer()
+// 需要安装 npm install -D @types/node (由于ts无法识别node.js文件，所以才需要下载声明文件)
+import { IncomingMessage, ServerResponse } from 'http';
+
+server.on('request', (request: IncomingMessage, response: ServerResponse) => {
+    // 如何才能找到 `request` 与 `response` 的构造函数（直接打印出来）
+    console.log('构造1：', request.constructor)
+    console.log('构造2：', response.constructor)
+
+    // request 的用法
+    console.log('method:', request.method);
+    console.log('url:', request.url);
+    console.log('headers:', request.headers);
+
+    const array = []
+    request.on('data', (chunk) => {
+        array.push(chunk)
+    })
+    request.on('end', () => {
+        // 根据官网查阅的资料：chunk 有三种类型，但是大部分情况下还是以 Buffe r为主
+        const requestBody = Buffer.concat(array).toString()
+
+        // response的用法
+        response.statusCode = 404;
+        // 使用 setHeader 可以设置请求头(可以随意设置key-value)
+        response.setHeader('X-frank', `I'm Frank`);
+        response.setHeader('Content-Type', 'image/png');
+
+        response.write('1\n');
+        console.log('获取请求体:', requestBody)
+
+        // 服务端必须得给出回应，不然客户端会一直出于等待阶段
+        // 需要写道回调函数内部，否则会
+        response.end('hi')
+    })
+
+
+})
+
+server.listen(8888)
